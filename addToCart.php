85689b237af816766aa28dcdfd42029b095e30d1
maria.afo1997@gmail.com <maria.afo1997@gmail.com>
2024-05-07 21:34:35 +0300

    fixed css path again again

--- addToCart.php
+++ addToCart.php
@@ -7,7 +7,7 @@
 	<title>addToCart</title>
 	
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
-	<link rel="stylesheet" href="/css/styles.css">
+	<link rel="stylesheet" href="./css/styles.css">
 </head>
 <body>
 
